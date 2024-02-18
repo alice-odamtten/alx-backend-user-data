5109da0dda96672e396615e3d490977eceaf0ad9
alice-odamtten <sakyibeaodamtten>
2024-02-18 01:30:15 +0000

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''returns false'''
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths is []:
+            return True
+
+        if path[-1] != "/":
+            path += "/"
+
+        if path not in excluded_paths:
+            return True
+        else:
+            return False
 
     def authorization_header(self, request=None) -> str:
         '''that returns None'''
