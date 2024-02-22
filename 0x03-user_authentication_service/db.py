b64e39e7f1a1d1d03216b8d81dc5506a58503797
alice-odamtten <sakyibeaodamtten>
2024-02-22 02:16:29 +0000

    task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -48,3 +48,14 @@ class DB:
         except InvalidRequestError:
             raise InvalidRequestError()
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        ''' then will update the user’s attributes as passed'''
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError()
+
+        self._session.commit()
