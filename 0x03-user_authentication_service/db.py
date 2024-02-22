7d2b85a6001c7f04dfeb1012337d4e36c7360048
alice-odamtten <sakyibeaodamtten>
2024-02-22 01:29:34 +0000

    task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -29,3 +29,10 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        ''' the add_user method'''
+        userr = User(email=email, hashed_password=hashed_password)
+        self._session.add(userr)
+        self._session.commit()
+        return userr
