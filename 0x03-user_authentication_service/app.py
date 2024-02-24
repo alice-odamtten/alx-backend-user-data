1f0cf910ea7eafd1d9323e6e28a4c474a3916ac0
alice-odamtten <sakyibeaodamtten>
2024-02-24 11:30:53 +0000

    task 12

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -23,6 +23,7 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+
 @app.route('/sessions', methods=['POST'])
 def login():
     '''login route'''
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -48,5 +49,16 @@ class Auth:
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        '''Get the user associated with the provided session ID.'''
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
