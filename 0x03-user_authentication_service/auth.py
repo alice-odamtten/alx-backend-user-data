19e4bb1a9d2c0925653e32c13313bf90148411b2
alice-odamtten <sakyibeaodamtten>
2024-02-24 12:10:41 +0000

    task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,3 +67,21 @@ class Auth:
         '''destroys the user's id'''
         if user_id:
             self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        '''used to reset the user password'''
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError()
+
+    def update_password(self, reset_token: str, password: str):
+        '''update user password'''
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id,
+                                 hashed_password=_hash_password(password),
+                                 reset_token=None)
